5bfa36900e1d3d89178974f3e80bd5de3051e930
varunthreja <varunthreja97@gmail.com>
2018-05-21 16:47:02 +1000

    Final Commit
    Project Completed

---
+++ External data/TextFileHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TextFileHandler : MonoBehaviour {
+
+    public string TextFile = "data";
+    string TextContent;
+
+    // Use this for initialization
+    void Start()
+    {
+        TextAsset txtAssets = (TextAsset)Resources.Load(TextFile);
+        TextContent = txtAssets.text;
+
+        print(TextContent);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
